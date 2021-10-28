53ae11be9d574f86496046cc1b3b15a6899c4e79
abbie <abbie@philippinedev.com>
2021-10-28 16:04:54 +0800

    -- generate model for user

--- spec/factories/admin_users.rb
+++
@@ -1,8 +0,0 @@
-FactoryBot.define do
-  factory :admin_user do
-    email { "MyString" }
-    password_digest { "MyString" }
-    reset_password_token { "MyString" }
-    reset_password_sent_at { "2021-10-28 15:41:23" }
-  end
-end
